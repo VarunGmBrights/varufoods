d7ca43de177d1660927d7ff63deb50b6afd00feb
Varun Gm <gmvarun60@gmail.com>
2026-02-22 13:40:30 +0530

    Update server.js
    new1

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,16 @@ const app = express()
 const port = process.env.PORT || 4000
 
 //middleware
-app.use(express.json())
-app.use(cors())
+// app.use(express.json())
+app.use(express.json({limit: '50mb'}))
+app.use(express.urlencoded({limit: '50mb', extended: true}))
+
+// app.use(cors())
+app.use(cors({
+  origin: "https://varufoods.onrender.com",
+  credentials: true
+}))
+
 
 // Serve uploaded images statically
 app.use('/uploads', express.static('uploads'))
@@ -37,7 +45,7 @@ app.get("/",(req,res)=>{
       res.send("API Working")
 })
 
-app.listen(port,()=>{
+// app.listen(port,()=>{
 
-    console.log(`Server Started on http://localhost:${port}`)
-})
+//     console.log(`Server Started on http://localhost:${port}`)
+// })
